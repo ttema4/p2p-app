17b4d8ffa392b150c090aafebd9ebf5924be19b7
Artem Ibragimov <65608749+ttema4@users.noreply.github.com>
2024-02-26 05:01:30 +0300

    total renaming and restructured

--- client/mainwindow_fwd.hpp
+++ client/mainwindow_fwd.hpp
@@ -2,8 +2,8 @@
 #include <unordered_map>
 
 std::unordered_map<std::string, std::string> include_map = {
-    {"Tinkoff", ":/bank_icons/source/Tink_bank.svg"},
-    {"Sber", ":/bank_icons/source/Sber_bank.svg"},
-    {"Alpha", ":/bank_icons/source/Alpha_bank.svg"},
-    {"Qiwi", ":/bank_icons/source/QIWI_bank.svg"},
-    {"SBP", ":/bank_icons/source/SBP_bank.svg"}};
+    {"Tinkoff", ":/resourses/bank_icons/Tink_bank.svg"},
+    {"Sber", ":/resourses/bank_icons/Sber_bank.svg"},
+    {"Alpha", ":/resourses/bank_icons/Alpha_bank.svg"},
+    {"Qiwi", ":/resourses/bank_icons/QIWI_bank.svg"},
+    {"SBP", ":/resourses/bank_icons/SBP_bank.svg"}};
