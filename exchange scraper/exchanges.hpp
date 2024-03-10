11264fe8917d1c1e8fa2a0ac182ab3771dcf4921
cierkai <nikita.vakushinnn@gmail.com>
2024-03-10 19:37:02 +0300

    creating json, implementing decimal

--- exchange scraper/exchanges.hpp
+++ exchange scraper/exchanges.hpp
@@ -1,7 +1,15 @@
+#ifndef EXCHANGES_HPP_
+#define EXCHANGES_HPP_
+
+#include "decimal/decimal.h"
 #include <iostream>
 #include <memory>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <random>
+
+enum class Exchanges { bybit_simulator };
 
 enum class Currencies { RUB };
 
@@ -10,35 +18,46 @@ enum class Coins { BTC, ETH, USDC, USDT };
 enum class Directions { buy, sell };
 
 struct order {
+  Exchanges exchange;
   Currencies currency;
   Coins coin;
   Directions direction;
-  double price;
+  dec::decimal<2> price;
   std::vector<std::string> payment_methods;
-  double lower_limit;
-  double upper_limit;
-  double available;
+  dec::decimal<2> lower_limit;
+  dec::decimal<2, dec::floor_round_policy> upper_limit;
+  dec::decimal<8> available;
   std::string link;
 
-  order(Currencies currency_, Coins coin_, Directions direction_, double price_,
-        std::vector<std::string> &payment_methods_, double lower_limit_,
-        double upper_limit_, double available_, std::string &link_);
+  order(Exchanges exchange_, Currencies currency_, Coins coin_,
+        Directions direction_, dec::decimal<2> price_,
+        std::vector<std::string> &payment_methods_,
+        dec::decimal<2> lower_limit_,
+        dec::decimal<2, dec::floor_round_policy> upper_limit_,
+        dec::decimal<8> available_, std::string &link_);
 };
 
-enum class Exchanges { bybit_simulator_ };
-
 struct exchange {
-  Exchanges name;
   std::vector<std::string> exchange_payment_methods;
   std::vector<std::unique_ptr<order>> orders;
 
   virtual ~exchange() = default;
 
-  virtual void update_orders() = 0;
+  virtual void update_exchange_orders() = 0;
 };
 
 struct bybit_simulator : exchange {
+  std::mt19937 gen;
+
   bybit_simulator();
 
-  void update_orders() override;
+  void update_exchange_orders() override;
 };
+
+std::string decimal2_to_string(dec::decimal<2> n);
+
+std::string decimal2frp_to_string(dec::decimal<2, dec::floor_round_policy> n);
+
+std::string decimal8_to_string(dec::decimal<8> n);
+
+#endif
--- exchange scraper/scraper.hpp
+++ exchange scraper/scraper.hpp
@@ -1,7 +1,19 @@
+#ifndef SCRAPER_HPP_
+#define SCRAPER_HPP_
+
 #include "exchanges.hpp"
+#include "magic_enum/magic_enum.hpp"
+#include "nlohmann/json.hpp"
+#include <fstream>
 
 struct scraper {
   std::vector<std::unique_ptr<exchange>> exchanges;
 
-  void add_exchange(exchange *exchange_);
+  void add_exchange(std::unique_ptr<exchange> exchange_);
+
+  void update_exchanges_orders();
+
+  void handle();
 };
+
+#endif
